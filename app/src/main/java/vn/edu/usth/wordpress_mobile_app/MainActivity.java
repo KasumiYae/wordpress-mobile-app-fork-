1b0233848af00b353990101c84e0e4eeffb7f711
HuyenanhBA12-006 <155504194+HuyenanhBA12-006@users.noreply.github.com>
2024-09-15 02:53:37 +0700

    change_activity_main2

--- app/src/main/java/vn/edu/usth/wordpress_mobile_app/MainActivity.java
+++ app/src/main/java/vn/edu/usth/wordpress_mobile_app/MainActivity.java
@@ -1,6 +1,8 @@
 package vn.edu.usth.wordpress_mobile_app;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -10,6 +12,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import vn.edu.usth.wordpress_mobile_app.Account.AccountActivity;
+import vn.edu.usth.wordpress_mobile_app.Media.MediaActivity;
+import vn.edu.usth.wordpress_mobile_app.Page.PageActivity;
+import vn.edu.usth.wordpress_mobile_app.Post.PostActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,16 +30,56 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main2);
-//        siteIconButton = findViewById(R.id.site_icon);
-//        titleTextView = findViewById(R.id.title);
-//        linkTextView = findViewById(R.id.link);
-//        postIcon = findViewById(R.id.post_icon);
-//        pageIcon = findViewById(R.id.page_icon);
-//        mediaIcon = findViewById(R.id.media_icon);
-//        commentIcon = findViewById(R.id.comment_icon);
-//        profileIcon = findViewById(R.id.profile_icon);
-//        settingIcon = findViewById(R.id.setting_icon);
-//        wordpressIcon = findViewById(R.id.wordpress_icon);
-//        plusButton = findViewById(R.id.plus_icon);
+        siteIconButton = findViewById(R.id.site_icon);
+        titleTextView = findViewById(R.id.title);
+        linkTextView = findViewById(R.id.link);
+        postIcon = findViewById(R.id.post_icon);
+        pageIcon = findViewById(R.id.page_icon);
+        mediaIcon = findViewById(R.id.media_icon);
+        commentIcon = findViewById(R.id.comment_icon);
+        profileIcon = findViewById(R.id.profile_icon);
+        settingIcon = findViewById(R.id.setting_icon);
+        wordpressIcon = findViewById(R.id.wordpress_icon);
+        plusButton = findViewById(R.id.fab);
+
+//        postIcon.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(MainActivity.this, PostActivity.class);
+//                startActivity(intent);
+//            }
+//        });
+//
+//        pageIcon.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(MainActivity.this, PageActivity.class);
+//                startActivity(intent);
+//            }
+//        });
+//
+//        mediaIcon.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(MainActivity.this, MediaActivity.class);
+//                startActivity(intent);
+//            }
+//        });
+//
+//        commentIcon.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(MainActivity.this, MediaActivity.class);
+//                startActivity(intent);
+//            }
+//        });
+//
+//        profileIcon.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(MainActivity.this, AccountActivity.class);
+//                startActivity(intent);
+//            }
+//        });
     }
 }
