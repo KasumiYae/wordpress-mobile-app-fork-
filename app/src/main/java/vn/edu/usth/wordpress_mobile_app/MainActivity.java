ecb003edfcf68f949540357a559805110b3109d8
HuyenanhBA12-006 <155504194+HuyenanhBA12-006@users.noreply.github.com>
2024-09-14 15:56:00 +0700

    add MainActivity

---
+++ app/src/main/java/vn/edu/usth/wordpress_mobile_app/MainActivity.java
@@ -0,0 +1,38 @@
+package vn.edu.usth.wordpress_mobile_app;
+
+import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageButton siteIconButton;
+    private TextView titleTextView, linkTextView;
+    private ImageView postIcon, pageIcon, mediaIcon, commentIcon, profileIcon, settingIcon, wordpressIcon;
+    private ImageView plusButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main2);
+        siteIconButton = findViewById(R.id.site_icon);
+        titleTextView = findViewById(R.id.title);
+        linkTextView = findViewById(R.id.link);
+        postIcon = findViewById(R.id.post_icon);
+        pageIcon = findViewById(R.id.page_icon);
+        mediaIcon = findViewById(R.id.media_icon);
+        commentIcon = findViewById(R.id.comment_icon);
+        profileIcon = findViewById(R.id.profile_icon);
+        settingIcon = findViewById(R.id.setting_icon);
+        wordpressIcon = findViewById(R.id.wordpress_icon);
+        plusButton = findViewById(R.id.plus_icon);
+    }
+}
