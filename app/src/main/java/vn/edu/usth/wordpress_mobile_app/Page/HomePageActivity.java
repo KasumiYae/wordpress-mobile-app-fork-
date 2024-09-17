f12fd03e2c923d9604ebc3524253035eef1113b9
AnhVan1111 <anhtv.ba12-007@st.usth.edu.vn>
2024-09-17 15:04:47 +0700

    add HomePage in Page

---
+++ app/src/main/java/vn/edu/usth/wordpress_mobile_app/Page/HomePageActivity.java
@@ -0,0 +1,33 @@
+package vn.edu.usth.wordpress_mobile_app.Page;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.RelativeLayout;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import vn.edu.usth.wordpress_mobile_app.R;
+
+public class HomePageActivity extends AppCompatActivity {
+    private ImageButton homepage;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.page_home_activity);
+        homepage = findViewById(R.id.backHomePage);
+        homepage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomePageActivity.this, PageActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
--- app/src/main/java/vn/edu/usth/wordpress_mobile_app/Page/PageFragment.java
+++ app/src/main/java/vn/edu/usth/wordpress_mobile_app/Page/PageFragment.java
@@ -10,17 +10,27 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 
 import vn.edu.usth.wordpress_mobile_app.R;
 
 
 public class PageFragment extends Fragment {
-    Button gotoPageHome, gotoPageIntroduction, gotoPageNews;
+    private RelativeLayout gotoPageHome, gotoPageIntroduction, gotoPageNews;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_page, container, false);
+        View view = inflater.inflate(R.layout.fragment_page, container, false);
+        gotoPageHome = view.findViewById(R.id.home_page);
+        gotoPageHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), HomePageActivity.class);
+                startActivity(intent);
+            }
+        });
+        return view;
     }
 }
