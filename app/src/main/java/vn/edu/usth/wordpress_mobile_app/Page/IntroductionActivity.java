b350c14449b737c147148de13e3c45cca13b9258
AnhVan1111 <anhtv.ba12-007@st.usth.edu.vn>
2024-09-18 13:09:49 +0700

    edit Page

---
+++ app/src/main/java/vn/edu/usth/wordpress_mobile_app/Page/IntroductionActivity.java
@@ -0,0 +1,26 @@
+package vn.edu.usth.wordpress_mobile_app.Page;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import vn.edu.usth.wordpress_mobile_app.R;
+
+public class IntroductionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.introduction_activity);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
