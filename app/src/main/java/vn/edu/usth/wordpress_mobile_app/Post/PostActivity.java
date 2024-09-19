6f573469d45531fa56042219a8451ecf06d6cb40
vietanhtran1403 <vietanhtran140323@gmail.com>
2024-09-20 00:12:33 +0700

    update delete2

--- app/src/main/java/vn/edu/usth/wordpress_mobile_app/Post/PostActivity.java
+++ app/src/main/java/vn/edu/usth/wordpress_mobile_app/Post/PostActivity.java
@@ -24,12 +24,7 @@ public class PostActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_post);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.post_layout), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-
+        
         PostFragmentAdapter adapter = new PostFragmentAdapter(getSupportFragmentManager());
 
         ViewPager pager = findViewById(R.id.pager);
