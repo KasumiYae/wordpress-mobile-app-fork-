e76d03e68693d92f6b7355466f84f468affc38d0
AnhVan1111 <anhtv.ba12-007@st.usth.edu.vn>
2024-11-06 15:24:23 +0700

    test HTTP request

--- app/src/main/java/vn/edu/usth/wordpress_mobile_app/Page/HomePageActivity.java
+++ app/src/main/java/vn/edu/usth/wordpress_mobile_app/Page/HomePageActivity.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,10 +15,22 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import vn.edu.usth.wordpress_mobile_app.R;
 
 public class HomePageActivity extends AppCompatActivity {
     private ImageButton homepage;
+    private TextView tv_data;
+    RequestQueue requestQueue;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,5 +42,34 @@ public class HomePageActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        requestQueue = Volley.newRequestQueue(HomePageActivity.this);
+        String url = "https://192.168.58.87:3000/post";
+
+
+        tv_data = findViewById(R.id.tv_data);
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject obj = new JSONObject(response);
+                    String name = obj.getString("content");
+                    tv_data.setText(name);
+
+                } catch (JSONException e) {
+                    throw new RuntimeException(e);
+                }
+
+                Toast.makeText(HomePageActivity.this, "CallApiDone", Toast.LENGTH_SHORT).show();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                tv_data.setText( "CallApiError");
+            }
+        });
+
+        requestQueue.add(stringRequest);
+
     }
 }
