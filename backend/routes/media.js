15e5c2fa964d251851313159ad2eb26504a0648d
khiemict0902 <khiem0902@gmail.com>
2024-11-08 17:29:39 +0700

    add api comment all

--- backend/routes/media.js
+++ backend/routes/media.js
@@ -1,1 +1,9 @@
 //cai nay cua tao ke di
+const express = require('express');
+
+const rout = express.Router();
+
+rout.use(express.static('../img'))
+
+
+module.exports = rout
--- backend/server.js
+++ backend/server.js
@@ -4,11 +4,13 @@ const app = express();
 const postRouter = require("./routes/post")
 const commentRouter = require("./routes/comment")
 const pageRouter = require("./routes/page")
+const media = require("./routes/media")
 
 
 app.use('/post', postRouter);
 app.use('/comment', commentRouter);
 app.use('/page', pageRouter);
+app.use('/media', express.static('img'));
 
 
 app.listen(3000, () => console.log('Example app is listening on port 3000.'));
